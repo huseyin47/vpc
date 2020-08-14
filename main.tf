18c0652b164a32401c91ae089e24de177f2990e8
Huseyin <huseyin.firatli@gmail.com>
2020-08-14 00:21:05 -0500

    need to work on it mor

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 0.12"
+}
+provider "aws" {
+  region = "${var.region}"
+}
+resource "aws_vpc" "main" {
+  cidr_block       = "${var.vpc_cidr}"
+  instance_tenancy = "default"
+  tags = {
+    Name     = "main"
+    Location = "Nusaybin"
+  }
+}
+resource "aws_subnet" "subnets" {
+  count             = "${length(data.aws_availability_zones.azs.names)}"
+  availability_zone = "${element(data.aws_availability_zones.azs.names, count.index)}"
+  vpc_id            = "${aws_vpc.main.id}"
+  cidr_block        = "${element(var.subnet_cidr, count.index)}"
+
+  tags = {
+    Name = "subnet-${count.index + 1}"
+  }
+}
